41b496a546c3b6c365e65bb7a96b95542e579a70
Vilped1 <vildeandrea01@gmail.com>
2024-11-26 13:48:17 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,12 @@ export default function Footer() {
         <footer>
             <p className="role">Journalist & fotograf: </p>
             <h5>Jenny Østreng & Ida Marie Olsen</h5>
+            <h5>Kandidatnr: 411 & Kandidatnr: 420</h5>
             <p className="date">Tirsdag 26. November 13:30</p>
         
             <p className="role" id="VA">Utvikler & designer: </p>
             <h5>Anne May Omberg & Vilde Andrea Fjeld Pedersen</h5>
+            <h5>Kandidatnr: 479 & Kandidatnr: 402</h5>
             <p className="date">Tirsdag 26. November 13:30</p>
         </footer>   
     )
